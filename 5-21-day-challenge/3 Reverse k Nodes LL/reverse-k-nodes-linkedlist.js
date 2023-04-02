fc288088b549500c049d39a376acec21877f2672
Gillian Trethewey <gilliantrethewey100@gmail.com>
2023-04-02 12:26:56 -0400

    alternate solution

--- 5-21-day-challenge/3 Reverse k Nodes LL/reverse-k-nodes-linkedlist.js
+++ 5-21-day-challenge/3 Reverse k Nodes LL/reverse-k-nodes-linkedlist.js
@@ -60,25 +60,60 @@ class ListNode {
 //   return head;
 // };
 
-var reverseInGroupsOfK = function(head, k) {
-  let stack = [];
-  let newNode = new ListNode(-1);
-  let temp = newNode;
-  
-  while(head) {
-      for(let i = 0; i < k && head; i++) {
-          stack.push(head);
-          head = head.next;
-      }
-      
-      if(stack.length === k) {
-          while(stack.length > 0) {
-              temp.next = stack.pop();
-              temp = temp.next;
-          }
-          temp.next = head;
-      }
+// var reverseInGroupsOfK = function(head, k) {
+//   let stack = [];
+//   let newNode = new ListNode(-1);
+//   let temp = newNode;
+
+//   while(head) {
+//       for(let i = 0; i < k && head; i++) {
+//           stack.push(head);
+//           head = head.next;
+//       }
+
+//       if(stack.length === k) {
+//           while(stack.length > 0) {
+//               temp.next = stack.pop();
+//               temp = temp.next;
+//           }
+//           temp.next = head;
+//       }
+//   }
+//   return newNode.next;
+// };
+
+function reverseInGroupsOfK(head, k) {
+  // 0 and 1 length lists do not require reversing, no matter what k is.
+  if (!head || !head.next || k === 1) return head;
+
+  // We'll need two pointers so that we can make changes to
+  // pointers without losing our position.
+  let prev = head;
+  let curr = prev.next;
+
+  // This makes sure that the first node will point to null
+  // when it is the last node after refersal.
+  prev.next = null;
+
+  // Now we'll count out k nodes to reverse.
+  let last = prev;
+  let count = 1;
+  while (curr && count < k) {
+    const temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+    count++;
   }
-  return newNode.next;
-};
+
+  // At this point we've reversed up to k nodes. If there
+  // is anything left, reverse that and set as the next
+  // node of the last of the current set.
+  if (curr) {
+    last.next = reverseInGroupsOfK(curr, k);
+  }
+
+  // Return the first node in this group of k.
+  return prev;
+}
 console.log(reverseInGroupsOfK(head, 2));
