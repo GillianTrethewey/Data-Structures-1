b212ad3cab1190e29827d2acee8874ed9606ca7e
Gillian Trethewey <gilliantrethewey100@gmail.com>
2023-04-02 13:53:55 -0400

    completed

--- 5-21-day-challenge/4 Toeplitz Matrix/toeplitz-matrix.js
+++ 5-21-day-challenge/4 Toeplitz Matrix/toeplitz-matrix.js
@@ -16,54 +16,19 @@ Example of a valid Toeplitz matrix:
 */
 
 function isToeplitz(m) {
-  let n = m[0].length;
-  let ideal = [];
-  for (let i = 0; i < n; i++) {
-    ideal.push(Array(n).fill(0));
-  }
-  let leftPointer = 0;
-  let rightPointer = n - 1;
-  let topPointer = 0;
-  let bottomPointer = n - 1;
+  
+  if (!m) return true
 
-  for (let i = 0; i < n; i++) {
-    for (let col = 0; col <= rightPointer; col++) {
-      ideal[topPointer + i][col] = col + 1;
+  for (let i = 0; i < m.length-1; i++) {
+    for (let j = i; j < m[i].length-1; j++) {
+      if (m[i][j] != m[i+1][j+1]) return false;
     }
   }
+  return true;
 
-  ideal[topPointer + 1].unshift(n + 1);
-  ideal[topPointer + 1].pop();
-
-  for (let col = 0; col <= rightPointer; col++) {
-    ideal[topPointer + 2][col] = col + 1;
-  }
-  ideal[topPointer + 2].unshift(n + 1);
-  ideal[topPointer + 2].pop();
-  ideal[topPointer + 2].unshift(n + 2);
-  ideal[topPointer + 2].pop();
-
-  for (let col = 0; col <= rightPointer; col++) {
-    ideal[topPointer + 3][col] = col + 1;
-  }
-  ideal[topPointer + 3].unshift(n + 1);
-  ideal[topPointer + 3].pop();
-  ideal[topPointer + 3].unshift(n + 2);
-  ideal[topPointer + 3].pop();
-  ideal[topPointer + 3].unshift(n + 3);
-  ideal[topPointer + 3].pop();
-
-  for (let col = 0; col < n; col++) {
-    ideal[topPointer + n - 1][col] = col + 1;
-  }
-  for (let i = 1; i < n; i++) {
-    ideal[topPointer + n - 1].unshift(n + i);
-    ideal[topPointer + n - 1].pop();
-  }
-  console.log(ideal);
 }
 
-let m = [
+let matrix = [
   [1, 2, 3, 4, 5],
   [6, 1, 2, 3, 4],
   [7, 6, 1, 2, 3],
@@ -71,4 +36,4 @@ let m = [
   [9, 8, 7, 6, 1],
 ];
 
-console.log(isToeplitz(m));
+console.log(isToeplitz(matrix));
