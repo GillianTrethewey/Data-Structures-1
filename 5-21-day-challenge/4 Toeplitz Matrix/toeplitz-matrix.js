09f26326b7a1b6f58a9c18b5fe048e33e5b7aac1
Gillian Trethewey <gilliantrethewey100@gmail.com>
2023-04-03 14:35:24 -0400

    day 4 amended

--- 5-21-day-challenge/4 Toeplitz Matrix/toeplitz-matrix.js
+++ 5-21-day-challenge/4 Toeplitz Matrix/toeplitz-matrix.js
@@ -15,17 +15,37 @@ Example of a valid Toeplitz matrix:
 
 */
 
-function isToeplitz(m) {
-  
-  if (!m) return true
+// function isToeplitz(m) {
 
-  for (let i = 0; i < m.length-1; i++) {
-    for (let j = i; j < m[i].length-1; j++) {
-      if (m[i][j] != m[i+1][j+1]) return false;
+//   if (!m) return true
+
+//   for (let i = 0; i < m.length-1; i++) {
+//     for (let j = i; j < m[i].length-1; j++) {
+//       if (m[i][j] != m[i+1][j+1]) return false;
+//     }
+//   }
+//   return true;
+
+// }
+
+function isToeplitz(matrix) {
+  // Here's that row-major traversal again, using two loops.
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[r].length; c++) {
+      const value = matrix[r][c];
+
+      // Here, we're going to loop up and left at a position
+      // diagonal from this one. Only do this if we're not going
+      // to index outside of the matrix. Check if that value isn't
+      // the same as this one.
+      if (r > 0 && c > 0 && matrix[r - 1][c - 1] !== value) {
+        return false;
+      }
     }
   }
-  return true;
 
+  // If we didn't find any problems, return true.
+  return true;
 }
 
 let matrix = [
