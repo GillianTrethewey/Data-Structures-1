36549e8fbdbbfb304ec0c654aebb8338bd1f8c3f
Gillian Trethewey <gilliantrethewey100@gmail.com>
2023-04-05 08:29:57 -0400

    alternative solution

--- 5-21-day-challenge/6 Find in Monotonic/findInMonotonic.js
+++ 5-21-day-challenge/6 Find in Monotonic/findInMonotonic.js
@@ -30,6 +30,44 @@ function findInMonotonic(matrix, k) {
   return false;
 }
 
+/*
+Alternative without flat:
+
+function findInMonotonic(matrix, k) {
+  const nrows = matrix.length;
+  if (nrows === 0) return false;
+
+
+  // Start at a corner that is the end of one dimension
+  // and start of another.
+  let row = nrows - 1;
+  let col = 0;
+  const ncols = matrix[row].length;
+
+
+  while (row >= 0 && col < ncols) {
+    const value = matrix[row][col];
+    if (value === k) {
+      // We found the target value!
+      return true;
+    } else if (value < k) {
+      // If the k is greater than this value, then it HAS
+      // to be at least one column over.
+      col++;
+    } else {
+      // If the k is less than this value, then it HAS
+      // to be at least one row up.
+      row--;
+    }
+  }
+
+
+  return false;
+}
+
+
+*/
+
 let matrix = [
   [0, 0, 0, 1],
   [1, 1, 1, 2],
