de2bae2566c7dd7618b2fb1eb98d660d1f326bad
Gillian Trethewey <gilliantrethewey100@gmail.com>
2023-04-08 14:54:04 -0400

    day 9 update

--- 5-21-day-challenge/9 Binary Search/binary-search.js
+++ 5-21-day-challenge/9 Binary Search/binary-search.js
@@ -25,3 +25,44 @@ function binarySearch(data, k) {
 }
 
 console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 5));
+/*
+Alternative:
+
+
+It’s very natural for many people to try to implement this with inclusive start and end indices. 
+The best answer (at least for languages that use 0-based indexing for arrays) turns out to be an 
+inclusive start and an exclusive end. 
+This leaves only one place in the code where I need to add one. Under more careful inspection, this 
+single plus one isn’t really even a fudge factor. 
+Remember since we want the start to be inclusive, we’re moving past the value we already know isn’t the target. 
+It’s not correcting index math, it’s making an explicit move based 
+on what we know about the possible location of the target.
+
+function binarySearch(data, k) {
+  // Inclusive starting index
+  let start = 0;
+  // Exclusive ending index
+  let end = data.length;
+
+
+  // The algorithm continues until we find what we're
+  // looking for OR we run out of places to look. If
+  // end greater than start, then we have at least one
+  // more place to check.
+  while (start < end) {
+    // Look in the middle between the start and end.
+    const mid = Math.floor((start + end) / 2);
+    const value = data[mid];
+
+
+    if (value === k) {
+      return mid;
+    } else if (value < k) {
+      start = mid + 1;
+    } else {
+      end = mid;
+    }
+  }
+  return -1;
+}
+*/
