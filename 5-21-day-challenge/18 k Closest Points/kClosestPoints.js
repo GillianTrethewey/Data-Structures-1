f954818ba1d767b47c868bf65d4280121b232231
Gillian Trethewey <gilliantrethewey100@gmail.com>
2023-04-17 16:29:13 -0400

    day 18

--- 5-21-day-challenge/18 k Closest Points/kClosestPoints.js
+++ 5-21-day-challenge/18 k Closest Points/kClosestPoints.js
@@ -7,4 +7,22 @@ find the k closest points to the origin (0, 0).
 
 */
 
-function kClosestPoints(points, k) {}
+
+function kClosestPoints(points, k) {
+    // Helper function to apply our euclidian distance formula
+    function getDistance([x1, y1], [x2, y2]) {
+      return Math.sqrt(((x1 - x2) ** 2) + ((y1 - y2) ** 2));
+    }
+  
+  
+    let closestPoints = [];
+  
+  
+    // Iterate through each set of points in our input array and get euc. distance at each step
+    for (const [x1, y1] of points) {
+      closestPoints.push([getDistance([x1, y1], [0, 0]), [x1, y1]]);
+    }
+  
+    // Use a naive/simple sort and slice to get the (k) closest points to origin
+    return closestPoints.sort((a, b) => a[0] - b[0]).slice(0, k).map((point) => point[1]);
+  }
