1a2ebbef1c47a04a3bf06624f7eb5336e4f4bea9
Gillian Trethewey <gilliantrethewey100@gmail.com>
2023-04-19 16:07:33 -0400

    day 20 updates

--- 5-21-day-challenge/20 Set Matrix Zeros/setMatrixZeros.js
+++ 5-21-day-challenge/20 Set Matrix Zeros/setMatrixZeros.js
@@ -19,4 +19,35 @@ Becomes:
 0 0 0
 */
 
-function setMatrixZeros(matrix) {}
+function setMatrixZeros(matrix) {
+  // Remember which rows and columns we need to zero out.
+  const rows = {};
+  const columns = {};
+
+  // Scan every position in the matrix and make note
+  // of the rows and columns that contain at least one zero.
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[0].length; c++) {
+      if (matrix[r][c] === 0) {
+        rows[r] = true;
+        columns[c] = true;
+      }
+    }
+  }
+
+  // Now go through the rows we need to zero.
+  for (const row in rows) {
+    for (let c = 0; c < matrix[0].length; c++) {
+      matrix[row][c] = 0;
+    }
+  }
+
+  // And finally the columns.
+  for (const column in columns) {
+    for (let r = 0; r < matrix.length; r++) {
+      matrix[r][column] = 0;
+    }
+  }
+
+  return matrix;
+}
